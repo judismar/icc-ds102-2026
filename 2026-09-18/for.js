f9334b85a561bed6c600a3831e75db7d915bdcbd
Judismar Arpini Junior <solid.judis@gmail.com>
2026-09-18 16:42:08 -0300

    Create for.js

---
+++ 2026-09-18/for.js
@@ -0,0 +1,16 @@
+let l = [2, 3, 5, 7, 11]
+
+for(let num of l)
+{
+    console.log(num);
+}
+
+for(let i = 0; i < 5; i++)
+{
+    console.log(l[i]);
+}
+
+for(let i = 4; i >= 0; i--)
+{
+    console.log(l[i]);
+}
